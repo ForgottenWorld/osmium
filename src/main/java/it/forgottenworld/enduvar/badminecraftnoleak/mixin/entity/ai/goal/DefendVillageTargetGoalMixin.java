2c3756eb4bd394e25a5a19e270eab0ddd6e30b85
Enduvar <70820452+Enduvar@users.noreply.github.com>
2020-10-08 22:32:35 +0200

    Fix DefendVillageTargetGoal memory leak

---
+++ src/main/java/it/forgottenworld/enduvar/badminecraftnoleak/mixin/entity/ai/goal/DefendVillageTargetGoalMixin.java
@@ -0,0 +1,18 @@
+package it.forgottenworld.enduvar.badminecraftnoleak.mixin.entity.ai.goal;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.ai.goal.DefendVillageTargetGoal;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+@Mixin(DefendVillageTargetGoal.class)
+public abstract class DefendVillageTargetGoalMixin extends TargetGoalMixin {
+
+    @Shadow private LivingEntity villageAgressorTarget;
+
+    @Override
+    public void resetTask() {
+        super.resetTask();
+        this.villageAgressorTarget = null;
+    }
+}
