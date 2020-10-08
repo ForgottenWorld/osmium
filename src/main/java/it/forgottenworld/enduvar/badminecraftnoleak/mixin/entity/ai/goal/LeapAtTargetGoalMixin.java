be0238a3746ea9fde4209df669395516d692a979
Enduvar <70820452+Enduvar@users.noreply.github.com>
2020-10-08 23:38:31 +0200

    Fix LeapAtTargetGoal memory leak

---
+++ src/main/java/it/forgottenworld/enduvar/badminecraftnoleak/mixin/entity/ai/goal/LeapAtTargetGoalMixin.java
@@ -0,0 +1,18 @@
+package it.forgottenworld.enduvar.badminecraftnoleak.mixin.entity.ai.goal;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.ai.goal.LeapAtTargetGoal;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+@Mixin(LeapAtTargetGoal.class)
+public abstract class LeapAtTargetGoalMixin extends GoalMixin {
+
+    @Shadow private LivingEntity leapTarget;
+
+    @Override
+    public void resetTask() {
+        super.resetTask();
+        this.leapTarget = null;
+    }
+}
