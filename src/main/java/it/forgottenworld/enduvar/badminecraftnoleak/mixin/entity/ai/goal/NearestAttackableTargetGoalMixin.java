7ea2370158a3ef1483d36a336af346bd94450a41
Enduvar <70820452+Enduvar@users.noreply.github.com>
2020-10-09 18:51:52 +0200

    Fix NearestAttackableTargetGoal memory leak

---
+++ src/main/java/it/forgottenworld/enduvar/badminecraftnoleak/mixin/entity/ai/goal/NearestAttackableTargetGoalMixin.java
@@ -0,0 +1,18 @@
+package it.forgottenworld.enduvar.badminecraftnoleak.mixin.entity.ai.goal;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.ai.goal.NearestAttackableTargetGoal;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+@Mixin(NearestAttackableTargetGoal.class)
+public abstract class NearestAttackableTargetGoalMixin extends TargetGoalMixin {
+
+    @Shadow protected LivingEntity nearestTarget;
+
+    @Override
+    public void resetTask() {
+        super.resetTask();
+        this.nearestTarget = null;
+    }
+}
