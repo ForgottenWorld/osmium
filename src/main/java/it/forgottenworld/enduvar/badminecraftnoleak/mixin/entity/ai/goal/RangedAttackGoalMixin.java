666be9646f540a6cfbe8cf576a44a2046cf5d19c
Enduvar <70820452+Enduvar@users.noreply.github.com>
2020-10-09 19:36:07 +0200

    Fix RangedAttackGoal memory leak

---
+++ src/main/java/it/forgottenworld/enduvar/badminecraftnoleak/mixin/entity/ai/goal/RangedAttackGoalMixin.java
@@ -0,0 +1,21 @@
+package it.forgottenworld.enduvar.badminecraftnoleak.mixin.entity.ai.goal;
+
+import net.minecraft.entity.MobEntity;
+import net.minecraft.entity.ai.goal.RangedAttackGoal;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(RangedAttackGoal.class)
+public abstract class RangedAttackGoalMixin extends GoalMixin {
+
+    @Shadow @Final private MobEntity entityHost;
+
+    @Inject(method = "resetTask", at = @At(value = "HEAD"))
+    private void onResetTask(CallbackInfo ci) {
+        this.entityHost.setAttackTarget(null);
+    }
+}
